1d1d7b3bede9af81a255c79b447b173195d0dcd3
Ayush <mail@ayuch.dev>
2026-07-18 19:02:09 +0530

    assignment: uca week-2 needle in the haystack
    Signed-off-by: Ayush <mail@ayuch.dev>

---
+++ algorithms/assignment_week_2/strstr.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int find_substring(const char *haystack, const char *needle) {
+    if (needle[0] == '\0') {
+        return 0;
+    }
+
+    for (int i = 0; haystack[i] != '\0'; i++) {
+        int j = 0;
+        while (needle[j] != '\0' && haystack[i + j] == needle[j]) {
+            j++;
+        }
+        if (needle[j] == '\0') {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        fprintf(stderr, "USAGE: %s [haystack] [needle]\n", argv[0]);
+        return 1;
+    }
+
+    int index = find_substring(argv[1], argv[2]);
+    printf("Output: %d\n", index);
+
+    return 0;
+}
